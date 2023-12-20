6009c07a7bad97b9d08a227bccdebf3978c19ad7
Mateus Lopes <mateuslopes92@gmail.com>
2023-12-20 10:59:03 -0300

    Added url and view to display data from database in html template

--- pocdrf/urls.py
+++ pocdrf/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from base import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.urls')),
+    path('api/', include('api.urls')),
+    path('base', views.base, name="base"),
 ]
