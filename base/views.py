f82ef35250a82916706b536877338e3d12813612
mateuslopes92 <mateuslopes92@gmai.com>
2023-12-20 10:59:03 -0300

    Added url and view to display data from database in html template

--- base/views.py
+++ base/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from api import views
+import requests
 
 # Create your views here.
+def base(request):
+  response = requests.get('http://127.0.0.1:8000/api/getData')
+  data = response.json()
+  return render(request, 'base.html', { 'data': data })
