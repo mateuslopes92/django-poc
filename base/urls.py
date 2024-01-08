ab628cacba95a734aee600ae1d1ba408a022125d
mateuslopes92 <mateuslopes92@gmai.com>
2024-01-08 10:49:54 -0300

    Create url to create item view

--- base/urls.py
+++ base/urls.py
@@ -6,4 +6,5 @@ app_name = 'base'
 urlpatterns = [
   path('', views.BaseView.as_view(), name='base'),
   path('<int:pk>/', views.SingleView.as_view(), name='single'),
+  path('add/', views.AddView.as_view(), name='add'),
 ]
