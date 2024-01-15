5c3534d0407ca939743308d001fe3ccf335ebc64
mateuslopes92 <mateuslopes92@gmai.com>
2024-01-14 23:04:03 -0300

    Added edit and delete to the crud

--- base/models.py
+++ base/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.urls import reverse
 
 class Item(models.Model):
-  id = models.IntegerField(primary_key=True, auto_created=True)
   title =  models.CharField(max_length=200)
   description =  models.TextField(null=True)
   created = models.DateTimeField(auto_now_add=True)
